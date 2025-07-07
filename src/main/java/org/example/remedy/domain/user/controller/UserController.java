3d4e01452dd8ca6661f6e66ffd8641e59f8050d6
허세진 <heosejin222@gmail.com>
2025-07-07 11:43:12 +0900

    feat(user): 본인 프로필 조회

--- src/main/java/org/example/remedy/domain/user/controller/UserController.java
+++ src/main/java/org/example/remedy/domain/user/controller/UserController.java
@@ -1,15 +1,25 @@
 package org.example.remedy.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.remedy.domain.user.dto.response.UserProfileResponse;
 import org.example.remedy.domain.user.service.UserService;
+import org.example.remedy.global.security.auth.AuthDetails;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/users")
 public class UserController {
     private final UserService userService;
 
-
+    @GetMapping("/users")
+    public ResponseEntity<UserProfileResponse> getMyProfile(@AuthenticationPrincipal AuthDetails authDetails) {
+        return ResponseEntity.ok(
+                userService.getMyProfile(authDetails.getUserId())
+        );
+    }
 }
--- src/main/java/org/example/remedy/domain/user/service/UserService.java
+++ src/main/java/org/example/remedy/domain/user/service/UserService.java
@@ -1,10 +1,26 @@
 package org.example.remedy.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.remedy.domain.user.domain.User;
+import org.example.remedy.domain.user.dto.response.UserProfileResponse;
+import org.example.remedy.domain.user.exception.UserNotFoundException;
+import org.example.remedy.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
+    private final UserRepository userRepository;
 
+    public UserProfileResponse getMyProfile(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("유저 못 찾음"));
+
+        return new UserProfileResponse(
+                user.getUsername(),
+                user.getProfileImage()
+        );
+    }
 }
