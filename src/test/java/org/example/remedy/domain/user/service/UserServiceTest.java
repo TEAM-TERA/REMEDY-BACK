092f0caf18acda101225c495bf5d23bc1af44446
허세진 <heosejin222@gmail.com>
2025-07-09 23:53:35 +0900

    test(user): 불필요한 import 제거

--- src/test/java/org/example/remedy/domain/user/service/UserServiceTest.java
+++ src/test/java/org/example/remedy/domain/user/service/UserServiceTest.java
@@ -11,8 +11,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDate;
-
 import static org.assertj.core.api.Assertions.*;
 import static org.example.remedy.domain.user.UserTestFactory.create;
 
