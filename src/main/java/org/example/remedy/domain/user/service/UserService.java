1f2c73c9f89df4d9332d6841d2618934936c000f
guhyunwoo <realization0327@gmail.com>
2025-07-13 11:55:42 +0900

    refactor(updateUserProfile): 기존 업데이트 메서드 변경

--- src/main/java/org/example/remedy/domain/user/service/UserService.java
+++ src/main/java/org/example/remedy/domain/user/service/UserService.java
@@ -1,17 +1,22 @@
 package org.example.remedy.domain.user.service;
 
+import io.minio.MinioClient;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.example.remedy.domain.minio.service.MinioService;
 import org.example.remedy.domain.user.domain.User;
 import org.example.remedy.domain.user.dto.request.UserProfileUpdateRequest;
+import org.example.remedy.domain.user.dto.response.UserProfileImageResponse;
 import org.example.remedy.domain.user.dto.response.UserProfileResponse;
 import org.example.remedy.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final MinioService minioService;
 
     public UserProfileResponse getMyProfile(User user) {
         return UserProfileResponse.newInstance(user);
@@ -21,5 +26,13 @@ public class UserService {
     public void updateUserProfile(UserProfileUpdateRequest req, User user) {
         user.updateProfile(req);
         userRepository.save(user);
+    }
+
+    @Transactional
+    public UserProfileImageResponse updateUserProfileImage(MultipartFile image, User user) {
+        String imageUrl = minioService.createImageUrl(image);
+        user.updateUserProfileImage(imageUrl);
+        userRepository.save(user);
+        return UserProfileImageResponse.newInstance(user);
     }
 }
