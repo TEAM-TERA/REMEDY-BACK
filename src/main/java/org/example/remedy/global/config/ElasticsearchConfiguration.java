a1d4ed8bb0742f9e7ae6731f17421ede1afa9c3e
guhyunwoo <realization0327@gmail.com>
2025-07-13 12:11:19 +0900

    refactor(ElasticsearchConfig): basePackageClasses를 실제 레포지토리 경로로 변경

--- src/main/java/org/example/remedy/global/config/ElasticsearchConfiguration.java
+++ src/main/java/org/example/remedy/global/config/ElasticsearchConfiguration.java
@@ -1,12 +1,13 @@
 package org.example.remedy.global.config;
 
+import org.example.remedy.domain.song.repository.SongElasticsearchRepository;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
-@EnableElasticsearchRepositories(basePackageClasses = ElasticsearchRepository.class)
+@EnableElasticsearchRepositories(basePackageClasses = SongElasticsearchRepository.class)
 public class ElasticsearchConfiguration extends org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration {
     @Override
     public ClientConfiguration clientConfiguration() {
