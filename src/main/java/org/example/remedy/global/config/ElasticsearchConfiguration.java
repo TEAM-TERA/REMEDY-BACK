40f0cac49127fd4901029dd73e0f2e3cfa3dc9f0
guhyunwoo <realization0327@gmail.com>
2025-07-15 09:51:36 +0900

    feat(Elasticsearch): properties, yml에 환경변수 작성

--- src/main/java/org/example/remedy/global/config/ElasticsearchConfiguration.java
+++ src/main/java/org/example/remedy/global/config/ElasticsearchConfiguration.java
@@ -1,14 +1,18 @@
 package org.example.remedy.global.config;
 
+import lombok.RequiredArgsConstructor;
 import org.example.remedy.domain.song.repository.SongElasticsearchRepository;
+import org.example.remedy.global.config.properties.ElasticsearchProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableElasticsearchRepositories(basePackageClasses = SongElasticsearchRepository.class)
 public class ElasticsearchConfiguration extends org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration {
+    private final ElasticsearchProperties elasticsearchProperties;
+
     @Override
     public ClientConfiguration clientConfiguration() {
         return ClientConfiguration.builder()
