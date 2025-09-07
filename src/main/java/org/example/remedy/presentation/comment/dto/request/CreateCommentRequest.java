76a9c5f1a4a2a570c9259f059c90b1b1a78f1837
허세진 <heosejin222@gmail.com>
2025-09-08 02:08:48 +0900

    feat: 댓글 생성 요청 DTO에 Validation 메시지 추가

--- src/main/java/org/example/remedy/presentation/comment/dto/request/CreateCommentRequest.java
+++ src/main/java/org/example/remedy/presentation/comment/dto/request/CreateCommentRequest.java
@@ -4,10 +4,10 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record CreateCommentRequest(
-        @NotBlank
-        @Size(max = 100)
+        @NotBlank(message = "내용은 필수입니다.")
+        @Size(max = 100, message = "댓글은 100자를 초과할 수 없습니다.")
         String content,
 
-        @NotBlank
+        @NotBlank(message = "드랍핑 ID는 필수입니다.")
         String droppingId
 ){}
