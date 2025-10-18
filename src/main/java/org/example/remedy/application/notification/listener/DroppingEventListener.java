692e283feae222d7283ad665423beb49fdaba646
허세진 <heosejin222@gmail.com>
2025-10-19 00:30:10 +0900

    refactor: 알림 전송 로직 비동기 처리로 수정

--- src/main/java/org/example/remedy/application/notification/listener/DroppingEventListener.java
+++ src/main/java/org/example/remedy/application/notification/listener/DroppingEventListener.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.example.remedy.application.notification.event.DroppingCreatedEvent;
 import org.example.remedy.application.notification.port.in.NotificationService;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +13,7 @@ public class DroppingEventListener {
 
     private final NotificationService notificationService;
 
+    @Async
     @EventListener
     public void onDroppingCreated(DroppingCreatedEvent event) {
         notificationService.sendDropNotification(event.userId(), event.songId());
