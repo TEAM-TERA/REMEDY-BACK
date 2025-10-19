6b8b251ebf031f8130a460fa04d0ce34f3a35ef0
허세진 <heosejin222@gmail.com>
2025-10-20 00:57:30 +0900

    refactor: Firebase 인증 파일 경로 하드코딩 수정

--- src/main/java/org/example/remedy/global/config/FirebaseConfig.java
+++ src/main/java/org/example/remedy/global/config/FirebaseConfig.java
@@ -4,22 +4,25 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${firebase.config.path}")
+    private String firebaseConfigPath;
+
     @PostConstruct
     public void init() throws IOException {
         if (FirebaseApp.getApps().isEmpty()) {
-            FileInputStream serviceAccount =
-                    new FileInputStream("src/main/resources/remedy-f8db9-firebase-adminsdk-fbsvc-b83376e99e.json");
+            ClassPathResource resource = new ClassPathResource(firebaseConfigPath);
 
             FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setCredentials(GoogleCredentials.fromStream(resource.getInputStream()))
                     .build();
 
             FirebaseApp.initializeApp(options);
