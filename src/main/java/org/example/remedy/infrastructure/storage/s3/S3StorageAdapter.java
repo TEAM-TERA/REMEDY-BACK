fedda80cb8b178684228ae35cf78441e85078cb9
허세진 <heosejin222@gmail.com>
2025-10-28 12:01:21 +0900

    refactor: 중복 업로드 코드를 uploadToS3()로 리팩터링

--- src/main/java/org/example/remedy/infrastructure/storage/s3/S3StorageAdapter.java
+++ src/main/java/org/example/remedy/infrastructure/storage/s3/S3StorageAdapter.java
@@ -9,7 +9,6 @@ import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.UUID;
 
@@ -24,30 +23,30 @@ public class S3StorageAdapter implements StoragePort {
     public String uploadFile(MultipartFile file) {
         try (InputStream inputStream = file.getInputStream()) {
             String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
-
-            s3Client.putObject(
-                    PutObjectRequest.builder()
-                            .bucket(s3Properties.getBucket())
-                            .key(fileName)
-                            .contentType(file.getContentType())
-                            .build(),
-                    RequestBody.fromInputStream(inputStream, file.getSize())
-            );
-
+            uploadToS3(inputStream, fileName, file.getContentType(), file.getSize());
             return createImageUrl(fileName);
         } catch (Exception e) {
             throw new RuntimeException("파일 업로드에 실패했습니다.", e);
         }
     }
 
-    private void putFile(MultipartFile file, String fileName, InputStream inputStream) throws IOException {
+    public String uploadFile(InputStream inputStream, String fileName, String contentType, long contentLength) {
+        try {
+            uploadToS3(inputStream, fileName, contentType, contentLength);
+            return createImageUrl(fileName);
+        } catch (Exception e) {
+            throw new RuntimeException("파일 업로드에 실패했습니다.", e);
+        }
+    }
+
+    private void uploadToS3(InputStream inputStream, String fileName, String contentType, long contentLength) {
         s3Client.putObject(
                 PutObjectRequest.builder()
                         .bucket(s3Properties.getBucket())
                         .key(fileName)
-                        .contentType(file.getContentType())
+                        .contentType(contentType)
                         .build(),
-                RequestBody.fromInputStream(inputStream, file.getSize())
+                RequestBody.fromInputStream(inputStream, contentLength)
         );
     }
 
@@ -56,21 +55,5 @@ public class S3StorageAdapter implements StoragePort {
             s3Properties.getBucket(),
             s3Properties.getRegion(),
             fileName);
-    }
-
-    public String uploadFile(InputStream inputStream, String fileName, String contentType, long contentLength) {
-        try {
-            s3Client.putObject(
-                    PutObjectRequest.builder()
-                            .bucket(s3Properties.getBucket())
-                            .key(fileName)
-                            .contentType(contentType)
-                            .build(),
-                    RequestBody.fromInputStream(inputStream, contentLength)
-            );
-            return createImageUrl(fileName);
-        } catch (Exception e) {
-            throw new RuntimeException("파일 업로드에 실패했습니다.", e);
-        }
     }
 }
