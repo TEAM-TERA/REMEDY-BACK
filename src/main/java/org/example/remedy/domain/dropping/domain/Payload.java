44e8f364cb56065d354a83be85431dbec8b320e1
guhyunwoo <realization0327@gmail.com>
2025-11-27 02:04:30 +0900

    fix: payload interface JsonType 선언으로 에러 해결

--- src/main/java/org/example/remedy/domain/dropping/domain/Payload.java
+++ src/main/java/org/example/remedy/domain/dropping/domain/Payload.java
@@ -1,4 +1,17 @@
 package org.example.remedy.domain.dropping.domain;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(
+	use = JsonTypeInfo.Id.NAME,
+	include = JsonTypeInfo.As.PROPERTY,
+	property = "type"
+)
+@JsonSubTypes({
+	@JsonSubTypes.Type(value = MusicDroppingPayload.class, name = "music"),
+	@JsonSubTypes.Type(value = PlaylistDroppingPayload.class, name = "playlist"),
+	@JsonSubTypes.Type(value = VoteDroppingPayload.class, name = "vote")
+})
 public interface Payload {
 }
